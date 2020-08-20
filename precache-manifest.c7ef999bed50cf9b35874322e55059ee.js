3069ee76f74c817fdd69054d56cc0365bdd7c7b3
jwood1290 <jwood1290@gmail.com>
2020-08-20 14:55:15 -0500

    Updates

---
+++ precache-manifest.c7ef999bed50cf9b35874322e55059ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a2c05df73f47ed73bbad9676e6f828",
+    "url": "/PyTron/index.html"
+  },
+  {
+    "revision": "d4b131ab5fd1b442fba7",
+    "url": "/PyTron/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "92ac16154d79b92811fc",
+    "url": "/PyTron/static/js/2.3bcb6e4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PyTron/static/js/2.3bcb6e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b131ab5fd1b442fba7",
+    "url": "/PyTron/static/js/main.73f0a08e.chunk.js"
+  },
+  {
+    "revision": "7c1eec189c78f3f2b106",
+    "url": "/PyTron/static/js/runtime-main.6e2a31ad.js"
+  }
+]);
