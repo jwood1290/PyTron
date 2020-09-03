1f5d687c10769b056d62ef417444ffcb55db40c4
jwood1290 <jwood1290@gmail.com>
2020-09-02 19:39:16 -0500

    Add version footer to login

--- src/Login.js
+++ src/Login.js
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import pkg_info from '../package.json';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -43,7 +44,11 @@ const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
     color: theme.palette.secondary.main,
-  }
+  },
+  endNote: {
+    position: 'absolute',
+    bottom: '10px',
+  },
 }));
 
 export default function Login(props) {
@@ -108,15 +113,16 @@ export default function Login(props) {
             >
               Sign In
             </Button>
-            {/*loading && <CircularProgress size={24} className={classes.buttonProgress} />}
-            <Backdrop className={classes.buttonProgress} open={loading}>
-              <CircularProgress color="inherit" />
-            </Backdrop>*/}
           </div>
         </form>
         <Backdrop className={classes.backdrop} open={props.failedLogin ? false:loading}>
           <CircularProgress color="inherit" />
         </Backdrop>
+        <Typography variant="body2" color="textSecondary" align="center" className={classes.endNote}>
+          {'v' + pkg_info.version + ' ('}
+          {new Date().getFullYear()}
+          {')'}
+        </Typography>
       </div>
     </Container>
   );
